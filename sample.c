9614aecf30744c35ef12b7c73b21e3eaa1daf262
ssheen906 <131233142+ssheen906@users.noreply.github.com>
2023-04-26 14:12:03 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -3,12 +3,10 @@
  int main() {
         printf("Hello, World!");
 	printf("Hello, IT World!");
-        return 0;
 	printf("Hello, this is Kendra");
 	printf("Hello, This is Royce Arpon");
 	printf("Im Bas");
-        printf("ssheen906);
+        printf("Hello World Orayt");
 	printf("Hello, this is Kendra");
-  return0;
 }
 
