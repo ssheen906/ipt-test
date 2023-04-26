2e1489f310da4743b2333d96c9f5300862f049b5
ssheen906 <131233142+ssheen906@users.noreply.github.com>
2023-04-26 14:16:18 +0800

    Update sample.c

--- sample.c
+++ sample.c
@@ -2,11 +2,10 @@
 
  int main() {
         printf("Hello, World!");
-	printf("Hello, IT World!");
-	printf("Hello, this is Kendra");
-	printf("Hello, This is Royce Arpon");
-	printf("Im Bas");
+        printf("Hello, IT World!");
+        printf("Hello, this is Kendra");
+        printf("Hello, This is Royce Arpon");
+        printf("Im Bas");
         printf("Hello World Orayt");
-	printf("Hello, this is Kendra");
+        printf("Hello, this is Kendra");
 }
-
